638883896b671c3ea5e43b5e7dce24a2787dc31f
Heyward Fann <fannheyward@gmail.com>
2019-07-10 17:56:58 +0800

    feat(downloader): http proxy support

--- src/downloader.ts
+++ src/downloader.ts
@@ -1,15 +1,37 @@
 import os from 'os';
 import tar from 'tar';
 import unzipper from 'unzipper';
+import tunnel from 'tunnel';
 import got from 'got';
 
 import { ExtensionContext, workspace } from 'coc.nvim';
+import { Agent } from 'http';
+
+function getAgent(): Agent | undefined {
+  let proxy = workspace.getConfiguration('http').get<string>('proxy', '');
+  if (proxy) {
+    let auth = proxy.includes('@') ? proxy.split('@', 2)[0] : '';
+    let parts = auth.length ? proxy.slice(auth.length + 1).split(':') : proxy.split(':');
+    if (parts.length > 1) {
+      let agent = tunnel.httpsOverHttp({
+        proxy: {
+          headers: {},
+          host: parts[0],
+          port: parseInt(parts[1], 10),
+          proxyAuth: auth
+        }
+      });
+      return agent;
+    }
+  }
+}
 
 async function getLatestVersion(): Promise<string> {
   let ver = '1.0.0';
   const apiURL = 'https://api.github.com/repos/latex-lsp/texlab/releases/latest';
   try {
-    const resp = await got(apiURL);
+    const agent = getAgent();
+    const resp = await got(apiURL, { agent });
     ver = JSON.parse(resp.body).tag_name;
   } catch (_e) {}
 
@@ -18,6 +40,7 @@ async function getLatestVersion(): Promise<string> {
 
 export async function downloadServer(context: ExtensionContext): Promise<void> {
   const ver = await getLatestVersion();
+  const agent = getAgent();
 
   const urls = {
     win32: `https://github.com/latex-lsp/texlab/releases/download/${ver}/texlab-x86_64-windows.zip`,
@@ -35,7 +58,7 @@ export async function downloadServer(context: ExtensionContext): Promise<void> {
   return new Promise((resolve, reject) => {
     try {
       got
-        .stream(url)
+        .stream(url, { agent })
         .on('downloadProgress', progress => {
           let p = (progress.percent * 100).toFixed(0);
           statusItem.text = `${p}% Downloading TexLab Server ${ver}`;
