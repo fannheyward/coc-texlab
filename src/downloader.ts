902ecbd78f8f6488600ff5a6b0e04dcac6a4436b
Heyward Fann <fannheyward@gmail.com>
2021-05-27 17:04:41 +0800

    feat: improve downloader
    close #222 #198

--- src/downloader.ts
+++ src/downloader.ts
@@ -1,10 +1,15 @@
 import { window, workspace } from 'coc.nvim';
+import { existsSync, mkdirSync, promises as fs } from 'fs';
 import { Agent } from 'http';
 import fetch from 'node-fetch';
-import os from 'os';
+import path from 'path';
+import { pipeline } from 'stream';
 import tar from 'tar';
 import tunnel from 'tunnel';
 import unzipper from 'unzipper';
+import { promisify } from 'util';
+
+const streamPipeline = promisify(pipeline);
 
 function getAgent(): Agent | undefined {
   let proxy = workspace.getConfiguration('http').get<string>('proxy', '');
@@ -25,18 +30,19 @@ function getAgent(): Agent | undefined {
   }
 }
 
-async function getLatestVersionTag(): Promise<string> {
+async function getLatestRelease(): Promise<{ ver: string; url: string; } | null> {
   const apiURL = 'https://api.github.com/repos/latex-lsp/texlab/releases/latest';
-  const agent = getAgent();
+  const resp = await fetch(apiURL, { agent: getAgent() });
+  if (!resp.ok) return null;
 
-  return fetch(apiURL, { agent })
-    .then(resp => resp.json())
-    .then(resp => {
-      return resp.tag_name;
-    })
-    .catch(() => {
-      return 'v1.0.0';
-    });
+  const ver = (await resp.json()).tag_name || 'v3.0.0';
+  const urls = {
+    win32: `https://github.com/latex-lsp/texlab/releases/download/${ver}/texlab-x86_64-windows.zip`,
+    linux: `https://github.com/latex-lsp/texlab/releases/download/${ver}/texlab-x86_64-linux.tar.gz`,
+    darwin: `https://github.com/latex-lsp/texlab/releases/download/${ver}/texlab-x86_64-macos.tar.gz`
+  };
+  const url = urls[process.platform];
+  return url ? { url, ver } : null;
 }
 
 export async function downloadServer(root: string): Promise<void> {
@@ -44,45 +50,36 @@ export async function downloadServer(root: string): Promise<void> {
   statusItem.text = 'Getting the latest version...';
   statusItem.show();
 
-  const ver = await getLatestVersionTag();
-  const agent = getAgent();
-
-  const urls = {
-    win32: `https://github.com/latex-lsp/texlab/releases/download/${ver}/texlab-x86_64-windows.zip`,
-    linux: `https://github.com/latex-lsp/texlab/releases/download/${ver}/texlab-x86_64-linux.tar.gz`,
-    darwin: `https://github.com/latex-lsp/texlab/releases/download/${ver}/texlab-x86_64-macos.tar.gz`
-  };
-  const url = urls[os.platform()];
-  if (!url) {
+  const release = await getLatestRelease();
+  if (!release) {
     statusItem.hide();
-    throw new Error(`Texlab doesn't support ${os.platform()} for now.`);
+    throw new Error(`Get TexLab release failed`);
   }
-  const extract = os.platform() === 'win32' ? () => unzipper.Extract({ path: root }) : () => tar.x({ C: root });
 
-  statusItem.text = `Downloading TexLab Server ${ver}`;
+  const resp = await fetch(release.url, { agent: getAgent() });
+  if (!resp.ok) {
+    statusItem.hide();
+    throw new Error('Download failed');
+  }
 
-  return new Promise((resolve, reject) => {
-    fetch(url, { agent })
-      .then(resp => {
-        let cur = 0;
-        const len = parseInt(resp.headers.get('content-length') || '', 10);
-        resp.body
-          .on('data', chunk => {
-            if (!isNaN(len)) {
-              cur += chunk.length;
-              const p = ((cur / len) * 100).toFixed(2);
-              statusItem.text = `${p}% Downloading TexLab Server ${ver}`;
-            }
-          })
-          .on('end', () => {
-            statusItem.hide();
-            resolve();
-          })
-          .pipe(extract());
-      })
-      .catch(e => {
-        statusItem.hide();
-        reject(e);
-      });
+  let cur = 0;
+  const len = Number(resp.headers.get('content-length'));
+  resp.body.on('data', (chunk: Buffer) => {
+    cur += chunk.length;
+    const p = ((cur / len) * 100).toFixed(2);
+    statusItem.text = `${p}% Downloading TexLab Server ${release.ver}`;
   });
+
+  const bin = process.platform === 'win32' ? 'texlab.exe' : 'texlab';
+  const tempRoot = path.join(root, 'download');
+  if (!existsSync(tempRoot)) {
+    mkdirSync(tempRoot);
+  }
+  const extract = process.platform === 'win32' ? () => unzipper.Extract({ path: tempRoot }) : () => tar.x({ C: tempRoot });
+  await streamPipeline(resp.body, extract());
+  await fs.unlink(path.join(root, bin)).catch((err) => {
+    if (err.code !== 'ENOENT') throw err;
+  });
+  await fs.rename(path.join(tempRoot, bin), path.join(root, bin));
+  statusItem.hide();
 }
